2fdde296ef67d367b366a023ba4f714f94725d3b
Дмитрий Шарыпов <45326015+DSharupov@users.noreply.github.com>
2018-11-27 21:18:05 +0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import time
+import telebot
+import configBot
+
+bot = telebot.TeleBot(configBot.token)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_msg(message):
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
---
+++ configBot.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Константа токена бота
+token = '613989227:AAEz0UBpRQ4jk-cJNBTWiEbxX69y7Y1nvGE'
